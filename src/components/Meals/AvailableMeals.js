ea426d936b6195644f609a881ad9094a143a349e
Seth Blanchard <nsethblanchard@gmail.com>
2021-11-16 14:26:27 -0500

    some work done on prices output

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Card from "../UI/Card";
 import Menu from "./Menu";
+import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 function AvailableMeals() {
-  const pupusas = Menu[0].pupusas.map((pupusa) => <li>{pupusa.name}</li>);
+  const pupusas = Menu[0].pupusas.map((pupusa) => (
+    <MealItem
+      key={pupusa.id}
+      name={pupusa.name}
+      description={pupusa.description}
+      single_price={pupusa.single_price}
+      price_for_3={pupusa.price_for_3}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -2,14 +2,19 @@ import React from "react";
 import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
-  const price = `$${props.price.toFixed(2)}`;
+  const singlePrice = `$${props.single_price.toFixed(2)}`;
+  const triplePrice = `$${props.price_for_3.toFixed(2)}`;
 
   return (
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <div className={classes.price_box}>
+          <div className={classes.price}>{singlePrice}</div>
+
+          <div className={classes.price}>{triplePrice}</div>
+        </div>
       </div>
       <div></div>
     </li>
